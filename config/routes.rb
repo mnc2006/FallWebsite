ea75ec128e719af30335a8fe6ba1184284edec64
mnc2006 <mnc2006@hotmail.com>
2015-11-02 08:40:17 -0500

    added controller through terminal

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root 'post#index'
 
   # Example of regular route:
-    get 'index/:id' => 'application#view'
+    get 'index/:id' => 'post#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
      #get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
