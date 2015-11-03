8b9be3137c8e2575f6a849ec5b5302be7eaca3f0
mnc2006 <mnc2006@hotmail.com>
2015-11-02 19:18:36 -0500

    updated from class

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -2,18 +2,17 @@ class PostController < ApplicationController
 
 
     def index
-      @posts = Post.all
+      @posts = Post.all.max_by{|p| p.created_at}
+      @posts = Post.all.reject{|p| p == @post}
     end
 
     def show
       @post = Post.find(params[:id].to_i)
+      @post = Post.all.reject{|p| p == @post}
+      render :index
     end
 
-    def sidebar
-      @sidebars
+  
 
-    end
-
-    
 
 end
